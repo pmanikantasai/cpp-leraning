b2b732d48a97dfc1fe29195d44957f914f1e2b00
Manikantasai <pallapumanikantasai@gmail.com>
2026-07-25 06:29:55 +0000

    commit it

---
+++ 01_varconst/01test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // how to make comments by using "single line uses //"
+
+    
+    /* it is uses to make multi line comment in cpp  for example 
+    (cout<<"this is line 1" << endl ;
+    // cout<<"this is line 2" << endl ;
+    cout<<"this is line 3" ;)
+*/
+    cout<<"this is line 1" << endl ;
+    // cout<<"this is line 2" << endl ;
+    cout<<"this is line 3" ;
+
+}
