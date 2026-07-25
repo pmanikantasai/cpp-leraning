e874ab45586206a3cede514dc35a875948f38e0a
Manikantasai <pallapumanikantasai@gmail.com>
2026-07-25 06:29:02 +0000

    commiting  varconst

---
+++ 01_varconst/02test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // variables declearation
+    // after variable can update what value we want
+    int num ;
+    num = 100;
+    // or 
+    int age = 23;
+    int my_age = 24;
+    int age1 =25;
+
+    // constant decleration by usin keyword like const
+
+    const int rollno = 210304124;
+    // const never change 
+    // we try to change we get error
+
+    cout<< rollno << endl ;
+    cout<< age ;
+
+    return 0;
+
+
+}
