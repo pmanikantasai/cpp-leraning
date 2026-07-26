117a85b7e8f3f102cc72491ab02bd175237ac4d8
Manikantasai <pallapumanikantasai@gmail.com>
2026-07-26 06:08:25 +0000

    commit it

---
+++ 02_datatypes/02strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    // string keyword use to store the string values 
+    string name = "Manikantasai";
+    string college ="parul university";
+    string address ="kphb,hyderabad-500072";
+
+    cout << name << "\n" << college << address ;
+
+    return 0;
+}
---
+++ 02_datatypes/03userinput.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    string name; 
+    int age;
+    long int phnno;
+
+    cout << "what is your name"<< endl;
+    cin >> name ;
+
+    cout << "tell me your age" << endl ;
+    cin >> age ;
+
+    cout << "and phn no" ;
+    cin >> phnno;
+    
+    return 0;
+
+}
+
+// how to run the user input is 
+// run as the file 
+// or  
+// open terminal and cd to the folder 
+// use _  ./file name like ( ./03userinput )
