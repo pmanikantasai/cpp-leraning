9682bd9da44a1cb7ff41a8dccdc82f274c2d74bf
Manikantasai <pallapumanikantasai@gmail.com>
2026-07-26 05:50:40 +0000

    commit it

---
+++ 02_datatypes/01permitive.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //permitive data types are
+    // int - both - , +
+    // float - like point values 2.98 , 9.0, 1.5
+    // boolean - true or false 
+    // char - used for single character like 'a' , 'A'
+    // double - used to store long percsion than float like 6.98765432
+    // void - used to store no value like empty
+    // long,long long,short,singed, unsinged
+    int myage ;
+    myage = 23;
+
+    char grade ='A';
+
+    float percentage = 9.99;
+
+    bool thevalue = true;
+
+    long int phnno = 9876543210;
+    // here "\n" used to get new line 
+    cout << myage << "\n"<< grade <<"\n"<< thevalue << "\n"<< phnno << endl;
+
+    return 0;
+
+
+}
