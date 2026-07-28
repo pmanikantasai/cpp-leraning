91ea30576e51181e8e1d96cb7e715f9fba0058a5
Manikantasai <pallapumanikantasai@gmail.com>
2026-07-28 04:17:06 +0000

    commit it

--- 06_functions/3function.cpp
+++ 06_functions/3function.cpp
@@ -6,9 +6,20 @@ int add (int a, int b=40){ // default parameter
     return a+b;
 }
 
+// Lambda expression
+auto greet = [] (){
+    cout << "Hello Man"<<endl;
+};
+
+auto square = [] (int n){
+    cout << n*n << endl;
+};
+
 int main (){
     int result = add(10);  // function calling
     cout << "function output is : "<< result << endl;
+    greet ();
+    square(12);
     return 0;
 }
 
